20b92b00bd05b7acb1ac0c3826e818db85d09dd7
erevis <erevis@live.unc.edu>
2021-03-29 19:43:37 -0400

    v 1.0

--- app.js
+++ app.js
@@ -28,8 +28,7 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
         const connection = await channel.join();
         connection.voice.setSelfDeaf(true);
 
-        //const dispatcher = connection.play(require("path").join(__dirname, './Sounds/' + fileName), { volume : 0.8 })
-        const dispatcher = connection.play('https://www.youtube.com/watch?v=AtZ6u1ijqow');
+        const dispatcher = connection.play(require("path").join(__dirname, './Sounds/' + fileName), { volume : 0.8 })
 
         dispatcher.on('start', () => { // song start
             console.log(fileName + ' is now playing!');
